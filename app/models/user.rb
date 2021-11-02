e832ef3fb9dcbd95c6dc646028d94a3643721986
Melvin Mathew <melvinmathew0102@gmail.com>
2021-11-02 14:23:10 +0530

    Added Rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\. [a-z]+)*\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50 }
   before_save :to_downcase
 
   private
 
-  def to_downcase
-    email.downcase!
-  end
+    def to_downcase
+      email.downcase!
+    end
 end
