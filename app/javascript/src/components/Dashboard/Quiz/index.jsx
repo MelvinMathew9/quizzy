07e8d1082286275ec5daa1a271d23c2e771bd7af
Melvin Mathew <melvinmathew0102@gmail.com>
2021-11-04 13:22:49 +0530

    Added Empty Quizzes page UI

--- app/javascript/src/components/Dashboard/Quiz/index.jsx
+++ app/javascript/src/components/Dashboard/Quiz/index.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import { Typography, Button } from "neetoui";
+
 const Quiz = () => {
-  return <>Quiz</>;
+  return (
+    <>
+      <div className="flex flex-col w-full py-10 md:px-5 px-4 space-y-6">
+        <Button label="Add new quiz" className="md:self-end self-center" />
+        <Typography style="h3" className="neeto-ui-text-gray-300 self-center">
+          You have not created any quiz
+        </Typography>
+      </div>
+    </>
+  );
 };
 
 export default Quiz;
