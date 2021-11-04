67172c39b5a5e3689fbc95460595314af1ce8814
Melvin Mathew <melvinmathew0102@gmail.com>
2021-11-04 19:18:10 +0530

    Added Plus icon to Button

--- app/javascript/src/components/Dashboard/Quiz/index.jsx
+++ app/javascript/src/components/Dashboard/Quiz/index.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
+import { Plus } from "neetoicons";
 import { Typography, Button } from "neetoui";
 
 const Quiz = () => {
   return (
     <>
       <div className="flex flex-col w-full py-10 md:px-5 px-4 space-y-6">
-        <Button label="Add new quiz" className="md:self-end self-center" />
+        <Button
+          label="Add new quiz"
+          iconPosition="left"
+          icon={() => <Plus size={18} />}
+          className="md:self-end self-center"
+        />
         <Typography style="h3" className="neeto-ui-text-gray-300 self-center">
           You have not created any quiz
         </Typography>
