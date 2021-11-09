400b577adbdeeb8b122b923b835eeeff861ef706
Melvin Mathew <melvinmathew0102@gmail.com>
2021-11-09 22:15:10 +0530

    Added unit test for options and quiz

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -3,7 +3,28 @@
 require "test_helper"
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(
+      first_name: "Sam", last_name: "Smith", email: "sam@example.com", password: "welcome",
+      password_confirmation: "welcome")
+    @quiz = Quiz.new(title: "test title", slug: "test-title", user: @user)
+    @question = Question.new(question: "Question 1", quiz: @quiz)
+  end
+
+  def test_question_should_not_be_blank
+    @question.question = ""
+    assert @question.invalid?
+  end
+
+  def test_question_should_not_be_valid_without_quiz
+    @question.quiz = nil
+    assert_not @question.save
+    assert_equal ["Quiz must exist"], @question.errors.full_messages
+  end
+
+  def test_question_should_not_exceed_maximum_length
+    @question.question = "a" * 251
+    assert_not @question.valid?
+    assert_equal ["Question is too long (maximum is 250 characters)"], @question.errors.full_messages
+  end
 end
