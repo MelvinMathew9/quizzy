6fac9d1800041ab4d085f8d073e6baa0afc73a99
Melvin Mathew <melvinmathew0102@gmail.com>
2021-11-16 21:13:43 +0530

    Added model vtests for attempt and attempt_answers

--- test/models/attempt_answer_test.rb
+++ test/models/attempt_answer_test.rb
@@ -3,7 +3,42 @@
 require "test_helper"
 
 class AttemptAnswerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(
+      first_name: "Sam", last_name: "Smith", email: "sam@example.com", password: "welcome",
+      password_confirmation: "welcome", role: "administrator")
+    @quiz = Quiz.new(title: "test title", slug: "test-title", user: @user)
+    @question = Question.new(question: "Question 1", quiz: @quiz)
+    @attempt = Attempt.new(quiz: @quiz, user: @user)
+    @attempt_answer = AttemptAnswer.new(answer: "test", question: @question, attempt: @attempt)
+  end
+
+  def test_attempt_answer_should_be_valid
+    @user.save
+    @quiz.save
+    @question.save
+    @attempt.save
+    assert @attempt_answer.valid?
+  end
+
+  def test_attempt_answer_should_not_be_valid_without_attempt
+    @attempt_answer.attempt = nil
+    assert_not @attempt_answer.save
+    assert_equal ["Attempt must exist"], @attempt_answer.errors.full_messages
+  end
+
+  def test_attempt_answer_should_not_be_valid_without_question
+    @attempt_answer.question = nil
+    assert_not @attempt_answer.save
+    assert_equal ["Question must exist"], @attempt_answer.errors.full_messages
+  end
+
+  def test_question_and_attempt_should_be_unique
+    @attempt.save
+    @question.save
+    @attempt_answer.save
+    attempt_answer_2 = AttemptAnswer.new(attempt: @attempt, question: @question)
+    assert_not attempt_answer_2.save
+    assert_equal ["Attempt has already been taken"], attempt_answer_2.errors.full_messages
+  end
 end
--- test/models/attempt_test.rb
+++ test/models/attempt_test.rb
@@ -3,7 +3,38 @@
 require "test_helper"
 
 class AttemptTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(
+      first_name: "Sam", last_name: "Smith", email: "sam@example.com", password: "welcome",
+      password_confirmation: "welcome")
+    @quiz = Quiz.new(title: "test", user: @user)
+    @attempt = Attempt.new(quiz: @quiz, user: @user)
+  end
+
+  def test_attempt_should_be_valid
+    @user.save
+    @quiz.save
+    assert @attempt.valid?
+  end
+
+  def test_attempt_should_not_be_valid_without_user
+    @attempt.user = nil
+    assert_not @attempt.save
+    assert_equal ["User must exist"], @attempt.errors.full_messages
+  end
+
+  def test_attempt_should_not_be_valid_without_quiz
+    @attempt.quiz = nil
+    assert_not @attempt.save
+    assert_equal ["Quiz must exist"], @attempt.errors.full_messages
+  end
+
+  def test_quiz_and_user_should_be_unique
+    @user.save
+    @quiz.save
+    @attempt.save
+    attempt_2 = Attempt.new(user: @user, quiz: @quiz)
+    assert_not attempt_2.save
+    assert_equal ["Quiz has already been taken"], attempt_2.errors.full_messages
+  end
 end
